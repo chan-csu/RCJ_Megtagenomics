e35e332e48c50726151da1e72a3b3b592f0a8638
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2023-01-19 22:45:13 -0700

    more megan plots

--- Data_Analysis/NMDS.R
+++ Data_Analysis/NMDS.R
@@ -1,11 +1,39 @@
 install.packages("vegan")
 install.packages("ggplot2")
 library(vegan)
-Sig_path=read.csv("~/Desktop/Merged_Pathabundance_Table_CPM_meta.tsv", header= TRUE,sep="\t")
+Sig_path=read.csv("Merged_Pathabundance_Table_CPM_meta.tsv", header= TRUE,sep="\t",row.names = 1)
 
-Metadata=read.csv("~/Desktop/metadata.csv",sep = ",")
+Metadata=read.csv("../output_all/metadata.csv",sep = ",")
 
-Table=Sig_path[,2:ncol(Sig_path)]
+Table=Sig_path[,2:ncol(Sig_path)-1]
+m_com = as.matrix(Table)
+nmds = metaMDS(t(m_com), distance = "bray")
+plot(nmds)
+Metadata=Metadata[-c(17),]
+data.scores = as.data.frame(scores(nmds)$sites)
+data.scores$Treatment_group = Metadata$Treatment_group
+data.scores$Samples=Metadata$Sample_ID
+
+
+library(ggplot2)
+
+xx = ggplot(data.scores, aes(x = NMDS1, y = NMDS2)) + 
+  geom_point(size = 4, aes( colour = Treatment_group))+
+  scale_color_manual(values=c("#999999", "#4C9A2A", "#1F75FE","#a000c8"))+
+  geom_text(aes(label=Samples), size=3,nudge_y = 0.02 )
+
+xx
+
+
+
+##### Taxa
+
+Sig_taxa=read.csv("megan/rel_freqs.csv", header= TRUE,sep=",",row.names = 1)
+
+Metadata=read.csv("../output_all/metadata.csv",sep = ",")
+Metadata=Metadata[-c(7,8,9,10,17),]
+
+Table=Sig_taxa[,1:ncol(Sig_taxa)]
 m_com = as.matrix(Table)
 nmds = metaMDS(t(m_com), distance = "bray")
 plot(nmds)
@@ -17,7 +45,8 @@ data.scores$Samples=Metadata$Sample_ID
 library(ggplot2)
 
 xx = ggplot(data.scores, aes(x = NMDS1, y = NMDS2)) + 
-  geom_point(size = 4, aes(  colour = Treatment_group))+
+  geom_point(size = 4, aes( colour = Treatment_group))+
+  scale_color_manual(values=c("#999999", "#4C9A2A", "#1F75FE","#a000c8"))+
   geom_text(aes(label=Samples), size=3,nudge_y = 0.02 )
 
 xx
